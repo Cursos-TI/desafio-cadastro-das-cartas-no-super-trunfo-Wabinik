560f4064e30e1fe20e3df8fbe17a335e8f120d9b
Wabinik <196389495+Wabinik@users.noreply.github.com>
2025-02-08 14:27:03 +0000

    Oi, era assim?

---
+++ Cadastro_Cartas_LGM.c
@@ -0,0 +1,231 @@
+#include <stdio.h>
+
+// Desafio Super Trunfo - Países
+// Tema 1 - Cadastro das Cartas
+// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
+// Siga os comentários para implementar cada parte do desafio.
+//Oito estados, de /A/ a /H/, e quatro cidades cada
+
+int main() {
+    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
+    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    
+    // Cadastro das Cartas:
+    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
+    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    
+    // Exibição dos Dados das Cartas:
+    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
+    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+  char Nome_Pais_1[16];
+
+    char Nome_estado_A01[64];
+   char Cidade_A01[32];
+  int Populacao_A01;
+  float Area_A01;
+  float PIB_A01;
+  int Pontos_Turisticos_A01;  //Não usar acentos nas variáveis
+
+   char Cidade_A02[32];
+  int Populacao_A02;
+  float Area_A02;
+  float PIB_A02;
+  int Pontos_Turisticos_A02;
+
+     char Cidade_A03[32];
+  int Populacao_A03;
+  float Area_A03;
+  float PIB_A03;
+  int Pontos_Turisticos_A03;
+
+     char Cidade_A04[32];
+  int Populacao_A04;
+  float Area_A04;
+  float PIB_A04;
+  int Pontos_Turisticos_A04;
+
+    char Nome_estado_B01[64];
+   char Cidade_B01[32];
+  int Populacao_B01;
+  float Area_B01;
+  float PIB_B01;
+  int Pontos_Turisticos_B01;  //Não usar acentos nas variáveis
+
+   char Cidade_B02[32];
+  int Populacao_B02;
+  float Area_B02;
+  float PIB_B02;
+  int Pontos_Turisticos_B02;
+
+     char Cidade_B03[32];
+  int Populacao_B03;
+  float Area_B03;
+  float PIB_B03;
+  int Pontos_Turisticos_B03;
+
+     char Cidade_B04[32];
+  int Populacao_B04;
+  float Area_B04;
+  float PIB_B04;
+  int Pontos_Turisticos_B04;
+
+    char Nome_estado_C01[64];
+   char Cidade_C01[32];
+  int Populacao_C01;
+  float Area_C01;
+  float PIB_C01;
+  int Pontos_Turisticos_C01;  //Não usar acentos nas variáveis
+
+   char Cidade_C02[32];
+  int Populacao_C02;
+  float Area_C02;
+  float PIB_C02;
+  int Pontos_Turisticos_C02;
+
+     char Cidade_C03[32];
+  int Populacao_C03;
+  float Area_C03;
+  float PIB_C03;
+  int Pontos_Turisticos_C03;
+
+     char Cidade_C04[32];
+  int Populacao_C04;
+  float Area_C04;
+  float PIB_C04;
+  int Pontos_Turisticos_C04;
+
+    char Nome_estado_D01[64];
+   char Cidade_D01[32];
+  int Populacao_D01;
+  float Area_D01;
+  float PIB_D01;
+  int Pontos_Turisticos_D01;  //Não usar acentos nas variáveis
+
+   char Cidade_D02[32];
+  int Populacao_D02;
+  float Area_D02;
+  float PIB_D02;
+  int Pontos_Turisticos_D02;
+
+     char Cidade_D03[32];
+  int Populacao_D03;
+  float Area_D03;
+  float PIB_D03;
+  int Pontos_Turisticos_D03;
+
+     char Cidade_D04[32];
+  int Populacao_D04;
+  float Area_D04;
+  float PIB_D04;
+  int Pontos_Turisticos_D04;
+
+    char Nome_estado_E01[64];
+   char Cidade_E01[32];
+  int Populacao_E01;
+  float Area_E01;
+  float PIB_E01;
+  int Pontos_Turisticos_E01;  //Não usar acentos nas variáveis
+
+   char Cidade_E02[32];
+  int Populacao_E02;
+  float Area_E02;
+  float PIB_E02;
+  int Pontos_Turisticos_E02;
+
+     char Cidade_E03[32];
+  int Populacao_E03;
+  float Area_E03;
+  float PIB_E03;
+  int Pontos_Turisticos_E03;
+
+     char Cidade_E04[32];
+  int Populacao_E04;
+  float Area_E04;
+  float PIB_E04;
+  int Pontos_Turisticos_E04;
+
+    char Nome_estado_F01[64];
+   char Cidade_F01[32];
+  int Populacao_F01;
+  float Area_F01;
+  float PIB_F01;
+  int Pontos_Turisticos_F01;  //Não usar acentos nas variáveis
+
+   char Cidade_F02[32];
+  int Populacao_F02;
+  float Area_F02;
+  float PIB_F02;
+  int Pontos_Turisticos_F02;
+
+     char Cidade_F03[32];
+  int Populacao_F03;
+  float Area_F03;
+  float PIB_F03;
+  int Pontos_Turisticos_F03;
+
+     char Cidade_F04[32];
+  int Populacao_F04;
+  float Area_F04;
+  float PIB_F04;
+  int Pontos_Turisticos_F04;
+
+    char Nome_estado_G01[64];
+   char Cidade_G01[32];
+  int Populacao_G01;
+  float Area_G01;
+  float PIB_G01;
+  int Pontos_Turisticos_G01;  //Não usar acentos nas variáveis
+
+   char Cidade_G02[32];
+  int Populacao_G02;
+  float Area_G02;
+  float PIB_G02;
+  int Pontos_Turisticos_G02;
+
+     char Cidade_G03[32];
+  int Populacao_G03;
+  float Area_G03;
+  float PIB_G03;
+  int Pontos_Turisticos_G03;
+
+     char Cidade_G04[32];
+  int Populacao_G04;
+  float Area_G04;
+  float PIB_G04;
+  int Pontos_Turisticos_G04;
+
+    char Nome_estado_H01[64];
+   char Cidade_H01[32];
+  int Populacao_H01;
+  float Area_H01;
+  float PIB_H01;
+  int Pontos_Turisticos_H01;  //Não usar acentos nas variáveis
+
+   char Cidade_H02[32];
+  int Populacao_H02;
+  float Area_H02;
+  float PIB_H02;
+  int Pontos_Turisticos_H02;
+
+     char Cidade_H03[32];
+  int Populacao_H03;
+  float Area_H03;
+  float PIB_H03;
+  int Pontos_Turisticos_H03;
+
+     char Cidade_H04[32];
+  int Populacao_H04;
+  float Area_H04;
+  float PIB_H04;
+  int Pontos_Turisticos_H04;
+
+
+
+printf("Olá, bem vindo(a) ao cadastro de cartas do Super Trunfo Países! \nDigite o nome de um(1) país para começar a cadastrar seus estados e cidades: \n"); 
+
+ scanf("%s", &Nome_Pais_1);
+ printf("O seu país será: %s \n", Nome_Pais_1);
+
+    return 0;
+}
